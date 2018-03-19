474607a1da6c9f15562b157369692f7f6352e127
Mac Decker <mcdecke@gmail.com>
2018-03-19 14:03:23 -0600

    committing

--- my-file.js
+++ my-file.js
@@ -1,5 +1,16 @@
 const {red, yellow} = require('chalk')
 const path = require('path')
 
-const filename = path.join(__dirname, 'test.txt')
-console.log(yellow(filename));
+const fs = require('fs')
+
+// const filename = path.join(__dirname, 'test.txt')
+// console.log(yellow(filename));
+
+fs.readdir(__dirname, (err, data) =>{
+  if(err){
+    console.log(red('ERROR!'));
+  }
+console.log(data);
+})
+
+console.log(yellow('Hello!'));
